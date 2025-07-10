6cb97adde0a1854f35995131afe8e891a30bdf01
SiranGuo <91914746+SiranGuo@users.noreply.github.com>
2025-07-09 22:38:35 -0700

    update some categories.

--- frontend/src/data/categories.js
+++ frontend/src/data/categories.js
@@ -1,4 +1,4 @@
-export const categories = ['眼影', '腮红', '提亮/修容', '化妆刷','染眉膏','眼线笔','眉笔','口红','粉扑' ];
+export const categories = ['眼影', '腮红', '提亮/修容', '化妆刷','染眉膏','眼线笔','眉笔','口红','粉扑','面膜','粉饼'];
 
 
 export default categories;
